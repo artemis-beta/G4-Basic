eda8eb09cfdf8a20013a2dfb7a9bcb6e1635186e
artemis-beta <KXZ186@student.bham.ac.uk>
2017-08-24 10:29:43 +0200

    Added volume iteration and drawing

--- g4_basic/__init__.py
+++ g4_basic/__init__.py
@@ -1,11 +1,27 @@
-import Geant4 as G4 
+import Geant4 as G4
 import g4py.ezgeom
 import g4py.NISTmaterials
 import g4py.ExN01pl
 import g4py.ParticleGun
 
+def init_ui_instance(theta_phi = (80,20), verbose=0):
+    ui = G4.G4UImanager.GetUIpointer()
+    ui.ApplyCommand("/run/verbose {}".format(verbose))
+    ui.ApplyCommand("/event/verbose {}".format(verbose))
+    ui.ApplyCommand("/tracking/verbose {}".format(verbose))
+    ui.ApplyCommand("/run/initialize")
+    ui.ApplyCommand("/run/verbose {}".format(verbose))
+    ui.ApplyCommand("/vis/open OGLIX")
+    ui.ApplyCommand("/vis/scene/create")
+    ui.ApplyCommand("/vis/scene/endOfEventAction accumulate")
+    ui.ApplyCommand("/vis/scene/add/volume")
+    ui.ApplyCommand("/vis/scene/add/trajectories")
+    ui.ApplyCommand("/vis/scene/add/hits")
+    ui.ApplyCommand("/vis/viewer/set/viewpointThetaPhi {} {}".format(*theta_phi))
+    return ui
+
 def parse_units(string):
-    dict_units = { 'MeV' : G4.MeV, 'GeV' : G4.GeV, 'keV' : G4.keV, 
+    dict_units = { 'MeV' : G4.MeV, 'GeV' : G4.GeV, 'keV' : G4.keV,
                    'cm' : G4.cm, 'mm' : G4.mm, 'm' : G4.m }
     out = 1
     if not isinstance(string, str):
@@ -21,38 +37,67 @@ def parse_units(string):
 
 class PGun:
   def __init__(self, name, particle, energy, position, momentum):
-      self._gun_obj = g4py.ParticleGun.Construct()
-      self._gun_obj.SetParticleByName(particle)
+      self.__gun_obj = g4py.ParticleGun.Construct()
+      self.__gun_obj.SetParticleByName(particle)
       positions = [ parse_units(i) for i in position ]
-      self._gun_obj.SetParticlePosition(G4.G4ThreeVector(*positions))
+      self.__gun_obj.SetParticlePosition(G4.G4ThreeVector(*positions))
       momenta = [ parse_units(j) for j in momentum ]
-      self._gun_obj.SetParticlePosition(G4.G4ThreeVector(*momenta))
+      self.__gun_obj.SetParticlePosition(G4.G4ThreeVector(*momenta))
 
-  def __call__(self):
-      return self._gun_obj
+  def get_gun(self):
+      return self.__gun_obj
+
+  __slots__ = ('__gun_obj')
 
 class Geometry:
+  g4py.ezgeom.Construct()
   def __init__(self, name, world_material, world_dimensions=None):
-      self._materials = g4py.NISTmaterials.Construct()
-      world_material = world_material.replace('G4','')
-      g4py.ezgeom.SetWorldMaterial(G4.G4Material.GetMaterial('G4_{}'.format(world_material)))
+      self.__materials = g4py.NISTmaterials.Construct()
+      world_material_G4 = world_material.replace('G4','')
+      self.name = name
+      self.world_material = world_material
+      g4py.ezgeom.SetWorldMaterial(G4.G4Material.GetMaterial('G4_{}'.format(world_material_G4)))
       if world_dimensions:
          world_dimensions = [ parse_units(i) for i in world_dimensions ]
          g4py.ezgeom.ResizeWorld(*world_dimensions)
-      self.volumes = {}
+      self.__volumes = {}
+
+  def __add__(self, other):
+      assert self.world_material == other.world_material, "World Materials do not match"
+      temp = Geometry('{}+{}'.format(self.name, other.name), self.world_material)
+      volumes = self.volumes.copy()
+      volumes.update(other.volumes)
+      temp.__volumes = volumes
+      return temp
 
   def add_volume(self, type_of_vol, name, material, dimensions, position, color=None):
-      self.volumes[name] = g4py.ezgeom.G4EzVolume(name)
+      self.__volumes[name] = g4py.ezgeom.G4EzVolume(name)
       dimensions = [ parse_units(i) for i in dimensions ]
       position   = [ parse_units(j) for j in position ]
-      getattr(self.volumes[name], 'Create{}Volume'.format(type_of_vol))(G4.G4Material.GetMaterial('G4_{}'.format(material)), *dimensions)
-      self.volumes[name].PlaceIt(G4.G4ThreeVector(*position))
+      getattr(self.__volumes[name], 'Create{}Volume'.format(type_of_vol))(G4.G4Material.GetMaterial('G4_{}'.format(material)), *dimensions)
+      self.__volumes[name].PlaceIt(G4.G4ThreeVector(*position))
       if color:
-        self.volumes[name].SetColor(G4.G4Color(*color))
-      
+        self.__volumes[name].SetColor(G4.G4Color(*color))
 
-if __name__ in "__main__":
-   G4.gRunManager.SetUserInitialization(G4.QGSP_BERT())
-   x = PGun('My Gun', 'e+', '3000MeV', ('0.0', '0.0', '-100.0cm'), ('0.0', '0.0', '2297.0'))
-   geo = Geometry('My Geo', 'AIR')
-   geo.add_volume('Box', 'My Box', 'Si', ('300.0mm', '300.0mm', '20.0mm'), ('0.0', '0.0', '500.0mm'), (124,252,0))
+  def resize_world(self, dimensions):
+      dimensions = [ parse_units(i) for i in dimensions ]
+      g4py.ezgeom.ResizeWorld(*world_dimensions)
+
+  def __iter__(self):
+      for key in self.__volumes:
+          yield self.__volumes[key]
+
+  def __getitem__(self, i):
+      if isinstance(i, int):
+         return self.__volumes[self.__volumes.keys()[i]]
+      else:
+         return self.__volumes[i]
+
+
+  def draw(self):
+      ui_vol = init_ui_instance()
+      ui_vol.ApplyCommand('/vis/drawVolume')
+
+  __slots__ = ('name', 'world_material', '__volumes', '__materials')
+
+
